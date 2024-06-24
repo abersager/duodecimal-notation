15c97cfe8bc5eca1a37312d94707c0461d985f10
Oliver Spindler <os@oliverspindler.com>
2024-06-25 01:07:44 +0200

    Apply modes

--- duodecimal_notation/convert.py
+++ duodecimal_notation/convert.py
@@ -1,5 +1,34 @@
 import typing
 
+Duodecimal = typing.Literal["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "X", "E"]
+Mode = typing.Literal["I", "II", "III", "IV", "V", "VI", "VII"]
+
+value_map = {
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "X": 10,
+    "E": 11,
+}
+
+reverse_value_map = {v: k for k, v in value_map.items()}
+
+mode_map = {
+    "I": 0,
+    "II": 2,
+    "III": 4,
+    "IV": 5,
+    "V": 7,
+    "VI": 9,
+    "VII": 11,
+}
 
 duodecimal_map_sharp = {
     "0": "C",
@@ -36,10 +65,18 @@ reverse_duodecimal_map_flat = {v: k for k, v in duodecimal_map_flat.items()}
 reverse_duodecimal_map = {**reverse_duodecimal_map_sharp, **reverse_duodecimal_map_flat}
 
 
-def from_duodecimal(note: str, hint: typing.Literal["sharp", "flat", None] = None):
+def from_duodecimal(
+    note: Duodecimal, hint: typing.Literal["sharp", "flat", None] = None
+):
     map = duodecimal_map_flat if hint == "flat" else duodecimal_map_sharp
     return map[note]
 
 
 def to_duodecimal(note: str):
     return reverse_duodecimal_map[note]
+
+
+def apply_mode(duodecimal: Duodecimal, mode: Mode) -> Duodecimal:
+    duodecimal_value = value_map[duodecimal]
+    mode_value = mode_map[mode]
+    return reverse_value_map[(duodecimal_value + mode_value) % 12]
--- duodecimal_notation/test_convert.py
+++ duodecimal_notation/test_convert.py
@@ -1,8 +1,8 @@
 import pytest
 
-from duodecimal_notation.convert import from_duodecimal, to_duodecimal
+from duodecimal_notation.convert import from_duodecimal, to_duodecimal, apply_mode
 
-test_cases = [
+convert_test_cases = [
     ("0", None, "C"),
     ("0", "sharp", "C"),
     ("0", "flat", "C"),
@@ -42,11 +42,108 @@ test_cases = [
 ]
 
 
-@pytest.mark.parametrize("duodecimal,hint, note", test_cases)
+@pytest.mark.parametrize("duodecimal,hint, note", convert_test_cases)
 def test_from_duodecimal(duodecimal, hint, note):
     assert from_duodecimal(duodecimal, hint) == note
 
 
-@pytest.mark.parametrize("duodecimal,hint,note", test_cases)
+@pytest.mark.parametrize("duodecimal,hint,note", convert_test_cases)
 def test_to_duodecimal(duodecimal, hint, note):
     assert to_duodecimal(note) == duodecimal
+
+
+mode_test_cases = [
+    ("0", "I", "0"),
+    ("0", "II", "2"),
+    ("0", "III", "4"),
+    ("0", "IV", "5"),
+    ("0", "V", "7"),
+    ("0", "VI", "9"),
+    ("0", "VII", "E"),
+    ("1", "I", "1"),
+    ("1", "II", "3"),
+    ("1", "III", "5"),
+    ("1", "IV", "6"),
+    ("1", "V", "8"),
+    ("1", "VI", "X"),
+    ("1", "VII", "0"),
+    ("2", "I", "2"),
+    ("2", "II", "4"),
+    ("2", "III", "6"),
+    ("2", "IV", "7"),
+    ("2", "V", "9"),
+    ("2", "VI", "E"),
+    ("2", "VII", "1"),
+    ("3", "I", "3"),
+    ("3", "II", "5"),
+    ("3", "III", "7"),
+    ("3", "IV", "8"),
+    ("3", "V", "X"),
+    ("3", "VI", "0"),
+    ("3", "VII", "2"),
+    ("4", "I", "4"),
+    ("4", "II", "6"),
+    ("4", "III", "8"),
+    ("4", "IV", "9"),
+    ("4", "V", "E"),
+    ("4", "VI", "1"),
+    ("4", "VII", "3"),
+    ("5", "I", "5"),
+    ("5", "II", "7"),
+    ("5", "III", "9"),
+    ("5", "IV", "X"),
+    ("5", "V", "0"),
+    ("5", "VI", "2"),
+    ("5", "VII", "4"),
+    ("6", "I", "6"),
+    ("6", "II", "8"),
+    ("6", "III", "X"),
+    ("6", "IV", "E"),
+    ("6", "V", "1"),
+    ("6", "VI", "3"),
+    ("6", "VII", "5"),
+    ("7", "I", "7"),
+    ("7", "II", "9"),
+    ("7", "III", "E"),
+    ("7", "IV", "0"),
+    ("7", "V", "2"),
+    ("7", "VI", "4"),
+    ("7", "VII", "6"),
+    ("8", "I", "8"),
+    ("8", "II", "X"),
+    ("8", "III", "0"),
+    ("8", "IV", "1"),
+    ("8", "V", "3"),
+    ("8", "VI", "5"),
+    ("8", "VII", "7"),
+    ("9", "I", "9"),
+    ("9", "II", "E"),
+    ("9", "III", "1"),
+    ("9", "IV", "2"),
+    ("9", "V", "4"),
+    ("9", "VI", "6"),
+    ("9", "VII", "8"),
+    ("X", "I", "X"),
+    ("X", "II", "0"),
+    ("X", "III", "2"),
+    ("X", "IV", "3"),
+    ("X", "V", "5"),
+    ("X", "VI", "7"),
+    ("X", "VII", "9"),
+    ("E", "I", "E"),
+    ("E", "II", "1"),
+    ("E", "III", "3"),
+    ("E", "IV", "4"),
+    ("E", "V", "6"),
+    ("E", "VI", "8"),
+    ("E", "VII", "X"),
+]
+
+
+@pytest.mark.parametrize("duodecimal, mode, result", mode_test_cases)
+def test_modes(duodecimal, mode, result):
+    assert apply_mode(duodecimal, mode) == result
+
+
+def test_composition():
+    assert from_duodecimal(apply_mode(to_duodecimal("C"), "VI")) == "A"
